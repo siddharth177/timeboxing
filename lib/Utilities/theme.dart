94f181e34938a81d368cb66ff4a6474addef2e3c
= <=>
2025-01-11 14:43:22 +0530

    theme data setup

---
+++ lib/Utilities/theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+var kLightThemeColors = {
+
+};
+
+
+var kDarkThemeColors = {
+
+};
+
+
+var kDarkThemeData = ThemeData().copyWith(
+  textTheme: GoogleFonts.urbanistTextTheme(),
+);
+
+var kLightThemeData = kDarkThemeData.copyWith(
+
+);
