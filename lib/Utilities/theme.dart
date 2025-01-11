9101f6898763a43d74d058040014ef41c021afe4
= <=>
2025-01-11 17:19:04 +0530

    adding color themes

--- lib/Utilities/theme.dart
+++ lib/Utilities/theme.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-final Map<String, Color> kLightThemeColors = {
-  'primaryColor': const Color(0XFF251506),
+final Map<String, Color> kDarkThemeColors = {
+  'darkBrownColor': const Color(0xFF251506),
+  'whiteBrownColor': const Color(0xFFF9F4F4),
+  'lightGreenColor': const Color(0xFF9BB068),
+  'lightOrangeColor': const Color(0xFFEF8833),
+  'lightBrownColor': const Color(0xFF4F3422),
+  'lightPurpleColor': const Color(0xFFA687FF)
 };
 
-final Map<String, Color> kDarkThemeColors = {
-  'primaryColor': const Color(0xFFF9F4F4),
+final Map<String, Color> kLightThemeColors = {
+  'whiteBrownColor': kDarkThemeColors['whiteBrownColor']!,
+  'lightGreenColor': kDarkThemeColors['lightGreenColor']!,
+  'lightOrangeColor': kDarkThemeColors['lightOrangeColor']!,
+  'lightBrownColor': kDarkThemeColors['lightBrownColor']!,
+  'lightPurpleColor': kDarkThemeColors['lightPurpleColor']!,
 };
 
 var kDarkThemeData = ThemeData().copyWith(
   textTheme: GoogleFonts.urbanistTextTheme(),
   colorScheme:
-      ColorScheme.fromSeed(seedColor: kLightThemeColors['primaryColor']!),
+      ColorScheme.fromSeed(seedColor: kDarkThemeColors['darkBrownColor']!),
 );
 
 var kLightThemeData = kDarkThemeData.copyWith(
   brightness: Brightness.dark,
   colorScheme:
-      ColorScheme.fromSeed(seedColor: kDarkThemeColors['primaryColor']!),
+      ColorScheme.fromSeed(seedColor: kLightThemeColors['whiteBrownColor']!),
 );
--- lib/screens/NoDisturbLandingScreen.dart
+++ lib/screens/NoDisturbLandingScreen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hugeicons/hugeicons.dart';
+import 'package:timeboxing/Utilities/date_time.dart';
 
 class NoDisturbLandingScreen extends StatefulWidget {
   const NoDisturbLandingScreen({super.key});
@@ -10,23 +10,61 @@ class NoDisturbLandingScreen extends StatefulWidget {
   }
 }
 
-class _NoDisturbLandingScreen extends State<NoDisturbLandingScreen>{
+class _NoDisturbLandingScreen extends State<NoDisturbLandingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Mon',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 48
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              weekDayShortNow(),
+              style: TextStyle(
+                fontSize: 48,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          )
-        ],
-      ),),
+            Text(
+              dayNow(),
+              style: TextStyle(
+                  fontSize: 80,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Expanded(
+              child: Stack(
+                children: [
+                  Positioned(
+                    top: 0,
+                    bottom: 0,
+                    left: 20,
+                    child: LayoutBuilder(builder: (context, constraints) {
+                      return Column(
+                        children: List.generate(
+                            (constraints.maxHeight / 20).floor(),
+                            (index) => Padding(
+                                  padding:
+                                      const EdgeInsets.symmetric(vertical: 5),
+                                  child: Container(
+                                    height: 10,
+                                    width: 2,
+                                    color: Colors.grey,
+                                  ),
+                                )),
+                      );
+                    }),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
