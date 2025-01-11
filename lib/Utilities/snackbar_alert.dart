9822d7dd1d0ca539febef8021159c428a3b3a60c
= <=>
2025-01-11 17:29:08 +0530

    snackbar dismiss button

--- lib/Utilities/snackbar_alert.dart
+++ lib/Utilities/snackbar_alert.dart
@@ -2,13 +2,31 @@ import 'package:flutter/material.dart';
 
 import '../models/alert_type.dart';
 
-void clearAndDisplaySnackbar(BuildContext context, String message, {int duration = 4, AlertType alertType = AlertType.info}) {
-  ScaffoldMessenger.of(context).clearSnackBars();
+void clearAndDisplaySnackbar(BuildContext context, String message,
+    {int duration = 4, AlertType alertType = AlertType.info}) {
+  clearSnackbar(context);
+  displaySnackBar(context, message,
+      duration: duration, alertType: AlertType.info);
+}
+
+void displaySnackBar(BuildContext context, String message,
+    {int duration = 4, AlertType alertType = AlertType.info}) {
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
       content: Text(message),
       duration: Duration(seconds: duration),
+      behavior: SnackBarBehavior.floating,
+      action: SnackBarAction(
+          label: 'Dismiss',
+          disabledTextColor: Colors.white, //TODO: design
+          onPressed: () {
+            clearSnackbar(context);
+          }),
       backgroundColor: Colors.blue, //TODO: change with alertType
     ),
   );
+}
+
+void clearSnackbar(BuildContext context) {
+  ScaffoldMessenger.of(context).clearSnackBars();
 }
