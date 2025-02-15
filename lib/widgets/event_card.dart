6fb7d3ba39625b259ea52fd26002e6ff03efea4c
Dipankar <dipankardutta1234567@gmail.com>
2025-02-15 12:22:06 +0530

    add event cards along with tasks
    tasks are now aligned with timeline

---
+++ lib/widgets/event_card.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:timeboxing/models/task.dart';
+
+class EventCard extends StatefulWidget {
+  final List<Task> tasks;
+  const EventCard({super.key, required this.tasks});
+
+  @override
+  State<EventCard> createState() => _EventCardState();
+}
+
+class _EventCardState extends State<EventCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+        padding: const EdgeInsets.all(30),
+        decoration: const BoxDecoration(
+          color: Colors.deepPurpleAccent,
+          // separate radius needed to merge continuous events spreading across different timeslots
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(8),
+            topRight: Radius.circular(8),
+            bottomRight: Radius.circular(8),
+            bottomLeft: Radius.circular(8),
+          ),
+        ),
+        child: Text(
+          widget.tasks[0].taskTitle,
+          style: const TextStyle(color: Colors.white),
+        ));
+  }
+}
--- lib/widgets/timeline.dart
+++ lib/widgets/timeline.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:timeboxing/models/task.dart';
+import 'package:timeboxing/widgets/event_card.dart';
 import 'package:timeline_tile/timeline_tile.dart';
 
 class Timeline extends StatelessWidget {
@@ -6,29 +8,38 @@ class Timeline extends StatelessWidget {
   final bool isLast;
   final bool isPast;
   final double gapHeight;
-  const Timeline(
-      {super.key,
-      required this.isFirst,
-      required this.isLast,
-      required this.isPast,
-      required this.gapHeight});
+  final List<Task> tasks;
+  const Timeline({
+    super.key,
+    required this.isFirst,
+    required this.isLast,
+    required this.isPast,
+    required this.gapHeight,
+    required this.tasks,
+  });
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: gapHeight, // keep it variable to change the gap between
       child: TimelineTile(
-          isFirst: isFirst,
-          isLast: isLast,
-          beforeLineStyle: const LineStyle(color: Colors.red),
-          indicatorStyle: IndicatorStyle(
-            width: 40,
-            color: Colors.blueAccent,
-            iconStyle: IconStyle(
-              iconData: Icons.timelapse,
-              color: Colors.white,
-            ),
-          )),
+        isFirst: isFirst,
+        isLast: isLast,
+        beforeLineStyle: const LineStyle(color: Colors.black, thickness: 4),
+        indicatorStyle: IndicatorStyle(
+          width: 40,
+          color: Colors.blueAccent,
+          iconStyle: IconStyle(
+            iconData: Icons.dangerous_outlined,
+            color: Colors.white,
+          ),
+        ),
+        afterLineStyle: const LineStyle(
+          color: Colors.red,
+          thickness: 4,
+        ),
+        endChild: EventCard(tasks: tasks),
+      ),
     );
   }
 }
